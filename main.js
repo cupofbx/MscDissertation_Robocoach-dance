519c9aea558873cfc7259ab0f89242896c457498
Shaoqi Zhang <15535700136@163.com>
2026-04-07 15:26:48 +0800

    fix UI layout stability

--- main.js
+++ main.js
@@ -96,6 +96,8 @@ const createPoseLandmarker = async () => {
         numPoses: 1
     });
 
+    document.getElementById("demos").style.display = "block";
+
     demosSection.classList.remove("invisible");
     console.log("双模型实例初始化成功！");
 };
